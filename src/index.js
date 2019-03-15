1715aa83a546799b8a916cd20784d219e03fe957
Dan Kiss <flowfast47@gmail.com>
2019-03-15 10:02:41 -0700

    change index to practice basic react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+//react dom gets your app to be rendered
 import ReactDOM from 'react-dom';
 import './index.css';
+//app is what is being rendered
 import App from './App';
+//service worker gets app to load faster 
 import * as serviceWorker from './serviceWorker';
 
+
+// const getCurrentDate = () => {
+//     const date = new Date();
+//     console.log(date.toDateString());
+//     return date.toDateString();
+// }
+
+// const greeting = React.createElement('h1', {}, 'Hello World');
+//text below is transpiled using babel
+//see you can invoke the function and get it's value returned for viewing
+// const greeting = <h1>Hello World current date {getCurrentDate()}</h1>
+// ReactDOM.render(greeting, document.getElementById('root'));
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
