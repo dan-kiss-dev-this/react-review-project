df94c66eb54e3c6f35fdf53507bc365f5fb6c9a6
Dan Kiss <flowfast47@gmail.com>
2019-03-15 10:25:15 -0700

    make a component and import

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,9 @@
+//import react to set up a component
+import React from 'react';
+
+//below is a functional component
+const Intro = props => (
+    <p>{props.message}</p>
+);
+
+export default Intro;
