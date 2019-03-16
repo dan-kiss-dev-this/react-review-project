775618bd8cb3ff4a002286931a701b7eaffab7db
Dan Kiss <flowfast47@gmail.com>
2019-03-15 17:15:39 -0700

    add series dynamically

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './index';
+
+const SeriesListItem = ({ series_single }) => (
+    <li>{series_single.show.name}</li>
+);
 
 const SeriesList = props => {
     return (
         <div>
-            <ul>
-                {props.list.map(series_single =>
-                    <li>{series_single.show.name}</li>
-                )}
+            <ul className="series-list">
+                {props.list.map((series_single, index) => (
+                    <SeriesListItem series_single={series_single} key={series_single.show.id}/>
+                ))}
             </ul>
         </div>
     )
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -6,19 +6,22 @@ class Series extends Component {
     state = {
         series: [],
       }
-    
-      componentDidMount() {
+
+    onSeriesInputChange = e => {
         //fetch will return a promise as a response object is returned
         //now we will need to parse results of response object, .json() takes a response stream and reads to completion, it returns a promise that resolves with the result of parsing the body text as json, note with .then we keep passing the resulting value to the next .then
-        fetch('http://api.tvmaze.com/search/shows?q=friends')
-          .then(response => response.json() )
-          .then(json => this.setState({ series: json}))
-      }
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+            .then(response => response.json() )
+            .then(json => this.setState({ series: json}))
+    }
 
     render() {
         return (
             <div>
                 <div>Length of series array is {this.state.series.length}</div>
+                <div>
+                    <input type='text' onChange={this.onSeriesInputChange} />
+                </div>
                 <SeriesList list={this.state.series}/>
             </div>
         );
