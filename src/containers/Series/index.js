f57d7b970615dc36c169cbc54c913d48dbf29496
Dan Kiss <flowfast47@gmail.com>
2019-03-17 11:10:47 -0700

    add conditional rendering

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -5,24 +5,35 @@ import SeriesList from '../../components/SeriesList/index';
 class Series extends Component {
     state = {
         series: [],
+        seriesName: '',
+        isFetching: false,
       }
 
     onSeriesInputChange = e => {
+        this.setState({ seriesName: e.target.value, isFetching: true });
         //fetch will return a promise as a response object is returned
         //now we will need to parse results of response object, .json() takes a response stream and reads to completion, it returns a promise that resolves with the result of parsing the body text as json, note with .then we keep passing the resulting value to the next .then
         fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
             .then(response => response.json() )
-            .then(json => this.setState({ series: json}))
+            .then(json => this.setState({ series: json, isFetching: false }))
     }
 
     render() {
+        const { series, seriesName, isFetching } = this.state;
+
         return (
             <div>
-                <div>Length of series array is {this.state.series.length}</div>
                 <div>
-                    <input type='text' onChange={this.onSeriesInputChange} />
+                    <input 
+                    value={seriesName}
+                    type='text' 
+                    onChange={this.onSeriesInputChange} />
                 </div>
-                <SeriesList list={this.state.series}/>
+                { series.length === 0 && seriesName.trim() === '' && <p>Enter series name</p>}
+                { series.length === 0 && seriesName.trim() !== ''  && <p>No TV series found</p>}
+                { isFetching && <p>Loading</p> }
+                { !isFetching && <SeriesList list={this.state.series}/> }
+                
             </div>
         );
     }
